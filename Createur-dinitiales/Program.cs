328b5fce71b0e6e603191aec4f0b86dfe54dec36
TheRealDAZL <0667750@cegepoutaouais.qc.ca>
2023-11-23 23:52:04 -0500

    Version fonctionnelle et légèrement plus optimisée de mon programme.

--- Createur-dinitiales/Program.cs
+++ Createur-dinitiales/Program.cs
@@ -1,9 +1,10 @@
-﻿// Titre : créateur d'initiales qui peut prendre des strings avec plein de caractères superflus, et qui redonne des initiales bien formatées
+﻿// Titre : créer des initiales à partir de strings pleins de caractères superflus, tout en redonnant des initiales bien formatées
+
+// NOTE IMPORTANTE : pour une raison que j'ignore les symboles "Ÿ", "Æ" et "Œ" ne sortent pas comme il le faut pendant l'exécution du programme.
+// Pourquoi? Peut-être parce que ces symboles sont mal supportés. C'est la seule explication que je vois en ce moment. J'ai donc enlevé ces symboles
+// du string[] SYMBOLES_AUTORISES
 
 // Déclarer puis initialiser les constantes et les variables
-// NOTE IMPORTANTE : pour une raison que j'ignore les symboles "Ÿ", "Æ" et "Œ" ne sortent pas comme il le faut pendant l'exécution du programme.
-// Pourquoi? Je ne sais pas, mais c'est peut-être parce que ces symboles sont mal supportés. C'est la seule explication que je vois en ce moment.
-// Je les ai donc enlevés des symboles présents dans le string[] SYMBOLES_AUTORISES
 string[] SYMBOLES_AUTORISES = new string[53] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "À", "Â", "Ä", "Ç", "É", "È", "Ê", "Ë", "Î", "Ï", "Ô", "Ö", "Ù", "Û", "Ü", "-", " " };
 string texte_valide = "";
 string initiales = "";
@@ -12,8 +13,8 @@ string initiales = "";
 while (texte_valide == "")
 {
     // Déclarer puis initialiser les variables
-    // Note -> Chaque valeur de la variable bool[] detection[i] est associée à une détection spécifique :
-    // detection[0] -> détection des espaces et des apostrophes
+    // Note : chaque valeur de bool[] detection est associée à une détection spécifique :
+    // detection[0] -> détection des espaces
     // detection[1] -> détection des tirets
     bool[] detection = new bool[2] { false, false };
     string texte_brut = "";
@@ -23,7 +24,7 @@ while (texte_valide == "")
 
     // Lire la console, ensuite enlever les tirets, les apostrophes et les espaces au début et à la fin de la valeur de texte_brut,
     // par la suite remplacer les apostrophes par des espaces, puis mettre la valeur lue en majuscules dans la variable texte_brut
-    // Note : en remplaçant les apostrophes par des espaces, on évite d'avoir à gêrer les apostrophes.
+    // Note : en remplaçant les apostrophes par des espaces, on évite d'avoir à gêrer les apostrophes en plus des autres symboles
     texte_brut = Console.ReadLine().Trim('-', '\'', ' ').Replace("'", " ").ToUpper();
 
     // Pour chaque symbole qui forme le contenu de texte_brut
@@ -32,7 +33,7 @@ while (texte_valide == "")
         // Déclarer et initialiser la variable symbole en prenant le symbole de texte_brut dont l'index est défini par la valeur de compteur
         string symbole = texte_brut.Substring(compteur, 1);
 
-        // Si la valeur de symbole est contenue dans les symboles autorisés, on concatène la valeur de ce symbole à la fin de la valeur de texte_valide
+        // Si la valeur de symbole est contenue dans les symboles autorisés, on ajoute la valeur de ce symbole à la fin de la valeur de texte_valide
         if (SYMBOLES_AUTORISES.Contains(symbole))
         {
             // Si le symbole actuel est différent d'un espace et d'un tiret
@@ -85,10 +86,11 @@ while (texte_valide == "")
 }
 
 // Déclarer et initialiser un string[] noms de la façon suivante :
-// Enlever les tirets, les apostrophes et les espaces au début et à la fin de la valeur de texte_valide,
-// et séparer la valeur lue à partir de chaque espace, en plaçant chaque mot qui est séparé par un espace
-// dans le string[] noms, tout en enlevant les espaces du résultat final
-string[] noms = texte_valide.Trim('-', '\'', ' ').Split(' ');
+// Enlever les tirets et les espaces au début et à la fin de la valeur tirée de
+// texte_valide, et séparer cette valeur à partir de chaque espace, en plaçant
+// chaque mot qui est séparé par un espace dans le string[] noms, tout en enlevant
+// les espaces du résultat final
+string[] noms = texte_valide.Trim('-', ' ').Split(' ');
 
 // Afficher ce message
 Console.WriteLine("Vos initiales sont :");
@@ -106,31 +108,13 @@ for (int element = 0; element < noms.Length; element++)
     // Si noms[element] contient un tiret
     else
     {
-        // Déclarer et initialiser la variable bool[] detection
-        bool[] detection = new bool[2] { false, false };
+        // Enlever les tirets au début et à la fin de la valeur tirée de noms[element], et séparer
+        // cette valeur à chaque tiret trouvé, en plaçant chaque mot qui est séparé par un tiret
+        // dans le string[] noms_composes, tout en enlevant les tirets du résultat final
+        string[] noms_composes = noms[element].Trim('-').Split('-');
 
-        // Si noms[element] commence par un tiret, enlever le tiret et changer la valeur de la variable detection[0] pour true
+        // Si noms[element] commence par un tiret, ajouter un tiret à la fin de initiales
         if (noms[element].StartsWith("-"))
-        {
-            noms[element] = noms[element].Remove(0, 1);
-
-            detection[0] = true;
-        }
-
-        // Si noms[element] fini par un tiret, enlever le tiret et changer la valeur de la variable detection[1] pour true
-        if (noms[element].EndsWith("-"))
-        {
-            noms[element] = noms[element].Remove(noms[element].Length - 1, 1);
-
-            detection[1] = true;
-        }
-
-        // Séparer chaque element du string[] noms pour chaque tiret trouvé, en plaçant chaque mot qui est séparé
-        // par un tiret dans le string array noms_composes, tout en enlevant les tirets du résultat final
-        string[] noms_composes = noms[element].Split('-');
-
-        // Si la variable detection[0] est vraie, ajouter un tiret à la fin de initiales
-        if (detection[0])
         {
             initiales += "-";
         }
@@ -138,9 +122,8 @@ for (int element = 0; element < noms.Length; element++)
         // Pour chaque element du string[] noms_composes trouvé, exécuter la boucle for suivante
         for (int element_2 = 0; element_2 < noms_composes.Length; element_2++)
         {
-
             // Si la variable noms_composes.Length contient plus d'un élément
-            if (noms_composes.Length - 1 > 0)
+            if (noms_composes.Length > 1)
             {
                 // Si on n'est pas rendus au dernier élément de noms_composes, on ajoute le premier symbole
                 // de noms_composes[element_2], un point et un tiret à la fin de initiales
@@ -165,15 +148,11 @@ for (int element = 0; element < noms.Length; element++)
             }
         }
 
-        // Si la variable detection[1] est vraie, ajouter un tiret à la fin de initiales
-        if (detection[1])
+        // Si noms[element] fini avec un tiret, ajouter un tiret à la fin de initiales
+        if (noms[element].EndsWith("-"))
         {
             initiales += "-";
         }
-
-        // Réinitialiser la variable bool[] detection
-        detection[0] = false;
-        detection[1] = false;
     }
 }
 
